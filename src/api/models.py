3bbad0c33bb79a02e5e1e554dce7b9332fc40228
Sabah Rahal <sabahrahal1998@gmail.com>
2022-10-31 22:17:20 +0000

    Fixed

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,149 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    def __init__(self, **kwargs):
+        self.username = kwargs['username']
+        self.password = kwargs['password']
 
+    @classmethod
+    def create(cls, **kwargs):
+        new_user = cls(**kwargs)
+        db.session.add(new_user) #INSERT INTO
+
+        try:
+            db.session.commit() #SE EJECUTA EL INSERT INTO
+            return new_user
+        except Exception as Error: 
+            raise Exception(Error.args[0], 400)
+    
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "id" : self.id,
+            "username" : self.username
         }
+        
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(120), unique=True, nullable=False)
+    terrain = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.BigInteger, unique=True, nullable=False) #PREGUNTAR A ERNESTO
+    diameter = db.Column(db.Integer, unique=True, nullable=False)
+    gravity = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, **kwargs): 
+        self.name = kwargs['name']
+        self.climate = kwargs['climate']
+        self.terrain = kwargs['terrain']
+        self.population = kwargs['population']
+        self.diameter = kwargs['diameter']
+        self.gravity = kwargs['gravity']
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_planet = cls(**kwargs)
+        db.session.add(new_planet) #INSERT INTO
+
+        try:
+            db.session.commit() #SE EJECUTA EL INSERT INTO
+            return new_planet
+        except Exception as Error: 
+            raise Exception(Error.args[0], 400)        
+    
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "climate" : self.climate,
+            "terrain" : self.terrain,
+            "population" : self.population,
+            "diameter" : self.diameter,
+            "gravity" : self.gravity
+        }
+        
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birth_year = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(10), unique=True, nullable=False)
+    height = db.Column(db.Integer, unique=True, nullable=False)
+    eye_color = db.Column(db.String(120), unique=True, nullable=False)
+    skin_color = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, **kwargs): 
+        self.name = kwargs['name']
+        self.birth_year = kwargs['birth_year']
+        self.gender = kwargs['gender']
+        self.height = kwargs['height']
+        self.eye_color = kwargs['eye_color']
+        self.skin_color = kwargs['skin_color']
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_character = cls(**kwargs)
+        db.session.add(new_character) #INSERT INTO
+
+        try: 
+            db.session.commit() #SE EJECUTA EL INSERT INTO
+            return new_character
+        except Exception as error: 
+            raise Exception(Error.args[0], 400)
+    
+    def serialize(self):
+        return{
+            "id" : self.id,
+            "name" : self.name,
+            "birth_year" : self.birth_year,
+            "gender" : self.gender,
+            "height" : self.height,
+            "eye_color" : self.eye_color,
+            "skin_color" : self.skin_color
+        }
+
+class Planet_favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id =  db.Column(db.Integer, db.ForeignKey('planet.id'))
+
+    def __init__(self, **kwargs): 
+            self.user_id = kwargs['user_id']
+            self.planet_id = kwargs['planet_id']
+    
+    @classmethod
+    def create(cls, **kwargs):
+        new_planet_favorite = cls(**kwargs)
+        db.session.add(new_planet_favorite) #INSERT INTO
+
+        try:
+            db.session.commit() #EJECUTA EL INSERT INTO
+            return new_character
+        except Exception as error: 
+            raise Exception(error.args[0], 400)
+
+    def serialize(self):
+        return{
+            "id" : self.id,
+            "user_id" : self.user_id,
+            "planet_id" : self.planet_id
+        }
+    
+    
+    
+
+
+# class Character_favorite(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, foreignKey('user.id'))
+#     character_id =  db.Column(db.Integer, foreignKey('character.id'))
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Planet, Character, Planet_favorite
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,103 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/users', methods=['POST', 'GET'])
+def handle_user():
+    if request.method == 'GET':
+        users = User.query.all()
+        users_dictionaries = []
+
+        for user in users: 
+            users_dictionaries.append(user.serialize())
+
+        return jsonify(users_dictionaries), 200
+    else: 
+        new_user_data = request.json
+        try:
+            new_user = User.create(**new_user_data)
+            return jsonify(new_user.serialize()), 201
+        except Exception as error: 
+            return jsonify(error.args[0], error.args[1] if len(error.args) > 1 else 500)
+
+@api.route('/user/<int:position>', methods=['DELETE'])
+def delete_user(position):
+    user = User.query.filter_by(id = position).delete()
+    db.session.commit()
+    return f"User with id = {position} has been deleted"
+
+@api.route('/planets', methods=['POST','GET'])
+def handle_planet():
+    if request.method == 'GET':
+            planets = Planet.query.all()
+            planets_dictionaries = []
+
+            for planet in planets: 
+                planets_dictionaries.append(planet.serialize())
+            
+            return jsonify(planets_dictionaries),200
+    else:
+            new_planet_data = request.json
+            try:
+                new_planet = Planet.create(**new_planet_data) 
+                return jsonify(new_planet.serialize()), 201
+            except Exception as error: 
+                return jsonify(error.args[0], error.args[1] if len(error.args) > 1 else 500)
+
+@api.route('/planet/<int:postion>', methods=['DELETE'])
+def delete_planet(position): 
+    planet = Planet.query.filter_by(id = position).delete()
+    db.session.commit()
+    return f"Planet with id = {position} has been deleted"
+
+@api.route('/characters', methods=['POST','GET'])
+def handle_character():
+    if request.method == 'GET':
+        characters = Character.query.all()
+        characters_dictionaries = []
+        
+        for character in characters:
+            characters_dictionaries.append(character.serialize())
+        
+        return jsonify(characters_dictionaries), 200
+    else: 
+        new_character_data = request.json
+        try: 
+            new_character = Character.create(**new_character_data)
+            return jsonify(new_character.serialize()), 201
+        except Exception as error: 
+                return jsonify(error.args[0], error.args[1] if len(error.args) > 1 else 500)
+
+@api.route('/favorites/characters', methods=['POST'])
+def handle_characters_favorites():
+    new_character_favorite_data = request.json
+    try: 
+        new_character_favorite = Character_favorite.create(**new_character_favorite_data)
+        return jsonify(new_character_favorite.serialize()), 201
+    except Exception as error: 
+        return jsonify(error.args[0], error.args[1] if len(error.args) > 1 else 500)
+
+@api.route('/character/<int:postion>', methods=['DELETE'])
+def delete_character(position): 
+    character = Character.query.filter_by(id = position).delete()
+    db.session.commit()
+    return f"Character with id = {position} has been deleted"
+
+@api.route('/favorites/planets/<int:position>', methods=['GET'])
+def handle_planets_favorites_by_id():
+    planets_favorites_by_id = Planet_favorite.query.filter_by(user_id = position)
+    planets_favorites_by_id_dictionaries = []
+    for planet in planets_favorites_by_id:
+            planets_favorites_by_id_dictionaries.append(planet.serialize())
+        
+    return jsonify(planets_favorites_by_id_dictionaries), 200
+
+
+@api.route('/favorites/planets', methods=['POST'])
+def handle_planets_favorites():
+        new_planet_favorite_data = request.json
+        try: 
+            new_planet_favorite = Planet_favorite.create(**new_planet_favorite_data)
+            return jsonify(new_planet_favorite.serialize()), 201
+        except Exception as error: 
+                return jsonify(error.args[0], error.args[1] if len(error.args) > 1 else 500)
